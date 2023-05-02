6d2f80e342ebb43e8e9fd1829baf4ab4ee104786
mandarinca <myler4ik@gmail.com>
2023-05-02 22:06:01 +0300

    Task-01 done

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,28 @@
+const startBtnEl = document.querySelector('[data-start]');
+const stopBtnEl = document.querySelector('[data-stop]');
+const bodyEl = document.querySelector('body');
 
+console.log(startBtnEl, stopBtnEl);
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
+
+let timerColor;
+
+startBtnEl.addEventListener('click', function () {
+  timerColor = setInterval(() => {
+    bodyEl.classList.add('body-class');
+    bodyEl.style.backgroundColor = getRandomHexColor();
+  }, 1500);
+  startBtnEl.setAttribute('disabled', true);
+  console.log(startBtnEl);
+});
+
+stopBtnEl.addEventListener('click', function () {
+  clearInterval(timerColor);
+  startBtnEl.removeAttribute('disabled');
+  console.log(startBtnEl);
+});
